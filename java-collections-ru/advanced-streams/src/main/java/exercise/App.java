1d6e07f1fddb9258e0b13e312f7560bf7db7b2e7
@hexlet/cli <support@hexlet.io>
2023-05-09 16:46:44 +0500

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -23,18 +23,5 @@ class App {
 
         return sj.toString();
     }
-
-    public static void main(String[] args) {
-        String file = "[program:prepare]\n" +
-                "command=sudo -HEu tirion /bin/bash -c 'cd /usr/src/app && make prepare'\n" +
-                "autorestart=false\n" +
-                "environment=\"X_FORWARDED_MAIL=tirion@google.com,X_FORWARDED_HOME=/home/tirion,language=en\"\n" +
-                "\n" +
-                "[program:http_server]\n" +
-                "command=sudo -HEu tirion /bin/bash -c 'cd /usr/src/app && make environment'\n" +
-                "environment=\"key5=value5,X_FORWARDED_var3=value,key6=value6\"";
-
-        System.out.println(getForwardedVariables(file));
-    }
 }
 //END
