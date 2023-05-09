0e98883193fea935192fee180b20c60dcd9f3fd8
@hexlet/cli <support@hexlet.io>
2023-05-09 16:57:00 +0500

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,7 +1,6 @@
 package exercise;
 
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
--- java-collections-ru/advanced-streams/src/test/java/exercise/AppTest.java
+++ java-collections-ru/advanced-streams/src/test/java/exercise/AppTest.java
@@ -1,8 +1,5 @@
 package exercise;
 
-// import java.util.ArrayList;
-// import java.util.List;
-
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
