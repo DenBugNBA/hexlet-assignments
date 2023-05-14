4b8081cdab917fb43725211f313fe2e0d622389e
@hexlet/cli <support@hexlet.io>
2023-05-15 00:42:21 +0500

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -4,8 +4,30 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.Objects;
 
 // BEGIN
 class App {
+    public static LinkedHashMap genDiff(Map<String, Object> map1, Map<String, Object> map2) {
+        LinkedHashMap<String, String> diffMap = new LinkedHashMap<>();
+
+        Set<String> sortedKeys = new TreeSet<>();
+        sortedKeys.addAll(map1.keySet());
+        sortedKeys.addAll(map2.keySet());
+
+        for (String key: sortedKeys) {
+            if (!map1.containsKey(key)) {
+                diffMap.put(key, "added");
+            } else if (!map2.containsKey(key)) {
+                diffMap.put(key, "deleted");
+            } else if (!Objects.equals(map1.get(key), map2.get(key))) {
+                diffMap.put(key, "changed");
+            } else {
+                diffMap.put(key, "unchanged");
+            }
+        }
+
+        return diffMap;
+    }
 }
 //END
